58239ca260dfbcd97796a93b57142c0dd23c08b1
karentrasporte <karentrasporte@gmail.com>
2026-05-13 21:47:39 +0800

    Added simple tests

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+from app import create_app
+
+def test_index_page():
+    app = create_app()
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+
+def test_learning_page():
+    app = create_app()
+    client = app.test_client()
+    response = client.get('/learning')
+    assert response.status_code == 200
